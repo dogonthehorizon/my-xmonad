0a886af5e847a05b9612c2b3a4aa60b0ff174c15
Fernando Freire <dogonthehorizon@gmail.com>
2022-12-30 12:14:59 -0800

    Reformat xmobar

--- my-xmobar/app/xmobar.hs
+++ my-xmobar/app/xmobar.hs
@@ -1,81 +1,96 @@
 module Main where
 
-import           Data.Color.Palette (Palette, defaultPalette)
+import Data.Color.Palette (Palette, defaultPalette)
 import qualified Data.Color.Palette as Palette
-import           Xmobar             (Config (..), Date (..), Monitors (..),
-                                     Runnable (Run), StdinReader (..),
-                                     XPosition (TopSize), defaultConfig, xmobar, Align(C))
+import Xmobar
+  ( Align (C),
+    Config (..),
+    Date (..),
+    Monitors (..),
+    Runnable (Run),
+    StdinReader (..),
+    XPosition (TopSize),
+    defaultConfig,
+    xmobar,
+  )
 
 config :: Palette -> Config
-config palette = defaultConfig
-    {
-    -- Appearance
-      font             = "xft:Roboto Condensed:style=Bold:size=13:antialias=true:hinting=true"
-    , bgColor          = Palette.backgroundColor palette
-    , fgColor          = Palette.foregroundColor palette
-    , position         = TopSize C 100 54
-    , iconRoot         = "/home/ffreire/git/my-xmonad/icons"
-
-    -- Layout
-    , sepChar          = "%"
-    , alignSep         = "}{"
-    , template         =
-        "  %UnsafeStdinReader% }{ %default:Master% | %wlp0s20f3wi% | %battery% | %date%  "
-    , lowerOnStart     = True
-    , hideOnStart      = False
-    , allDesktops      = True
-    , overrideRedirect = True
-    , pickBroadest     = False
-    , persistent       = True
-
-    -- Command Config
-    , commands         =
-        [ Run $ Battery
-            [ "--template"
-            , "<acstatus> <left>% (<timeleft>)"
-            , "--Low"
-            , "30"
-            , "--High"
-            , "80"
-            , "-p"
-            , "3"
-            , "--low"
-            , Palette.red palette
-            , "--"
-            , "-O"
-            , "<icon=battery-mid-charging.xpm/>"
-            , "-i"
-            , "<icon=battery-charging.xpm/>"
-            , "-o"
-            , "<icon=battery-mid.xpm/>"
-            ]
-            50
-        , Run UnsafeStdinReader
-        , Run $ Wireless
-            "wlp0s20f3"
-            [ "--template"
-            , "<icon=connected.xpm/> <essid> <quality>"
-            , "--high"
-            , Palette.green palette
-            , "--normal"
-            , Palette.yellow palette
-            , "--low"
-            , Palette.red palette
-            ]
-            30
-        , Run $ Date "<icon=calendar.xpm/> %R %a %Y-%m-%d " "date" 1
-        , Run $ Volume
-            "default"
-            "Master"
-            [ "--template"
-            , "<status> <volume>%"
-            , "--"
-            , "-O"
-            , "<icon=volume-on.xpm/>"
-            , "-o"
-            , "<icon=volume-muted.xpm/>"
-            ]
-            10
+config palette =
+  defaultConfig
+    { -- Appearance
+      font = "xft:JetBrainsMono NF:weight=bold:pixelsize=40:antialias=true:hinting=true",
+      additionalFonts =
+        [ "xft:Mononoki:pixelsize=40",
+          "xft:Font Awesome 6 Free Solid:pixelsize=40",
+          "xft:Font Awesome 6 Brands:pixelsize=40",
+          "xft:JetBrainsMono NF:weight=bold:pixelsize=35:antialias=true:hinting=true"
+        ],
+      bgColor = Palette.backgroundColor palette,
+      fgColor = Palette.foregroundColor palette,
+      position = TopSize C 100 54,
+      iconRoot = "/home/ffreire/git/my-xmonad/icons",
+      -- Layout
+      sepChar = "%",
+      alignSep = "}{",
+      template =
+        "  %UnsafeStdinReader% }{ %default:Master% | %wlp0s20f3wi% | %battery% | %date%  ",
+      lowerOnStart = True,
+      hideOnStart = False,
+      allDesktops = True,
+      overrideRedirect = True,
+      pickBroadest = False,
+      persistent = True,
+      -- Command Config
+      commands =
+        [ Run $
+            Battery
+              [ "--template",
+                "<acstatus> <left>% (<timeleft>)",
+                "--Low",
+                "30",
+                "--High",
+                "80",
+                "-p",
+                "3",
+                "--low",
+                Palette.red palette,
+                "--",
+                "-O",
+                "<icon=battery-mid-charging.xpm/>",
+                "-i",
+                "<icon=battery-charging.xpm/>",
+                "-o",
+                "<icon=battery-mid.xpm/>"
+              ]
+              50,
+          Run UnsafeStdinReader,
+          Run $
+            Wireless
+              "wlp0s20f3"
+              [ "--template",
+                "<icon=connected.xpm/> <essid> <quality>",
+                "--high",
+                Palette.green palette,
+                "--normal",
+                Palette.yellow palette,
+                "--low",
+                Palette.red palette
+              ]
+              30,
+          Run $ Date "<icon=calendar.xpm/> %R %a %Y-%m-%d " "date" 1,
+          Run $
+            Volume
+              "default"
+              "Master"
+              [ "--template",
+                "<status> <volume>%",
+                "--",
+                "-O",
+                "<icon=volume-on.xpm/>",
+                "-o",
+                "<icon=volume-muted.xpm/>"
+              ]
+              10
         ]
     }
 
