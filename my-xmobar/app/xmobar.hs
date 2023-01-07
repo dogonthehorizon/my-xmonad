17d493497b54f2c6eeedeb8c025be24742ca1cba
Fernando Freire <dogonthehorizon@gmail.com>
2023-01-07 09:57:04 -0800

    Switch from icons to font-awesome glyphs in status bar

--- my-xmobar/app/xmobar.hs
+++ my-xmobar/app/xmobar.hs
@@ -20,15 +20,12 @@ config palette =
     { -- Appearance
       font = "Cantarell Bold 30",
       additionalFonts =
-        [ "Mononoki 40",
-          "Font Awesome 6 Free Solid 30",
-          "Font Awesome 6 Brands 30",
-          "Cantarell Bold 30"
+        [ "Font Awesome 6 Free Solid 22",
+          "Font Awesome 6 Brands 22"
         ],
       bgColor = Palette.backgroundColor palette,
       fgColor = Palette.foregroundColor palette,
-      position = TopSize C 100 55,
-      iconRoot = "/home/ffreire/git/my-xmonad/icons",
+      position = TopSize C 100 65,
       -- Layout
       sepChar = "%",
       alignSep = "}{",
@@ -51,23 +48,27 @@ config palette =
                 "--High",
                 "80",
                 "-p",
-                "3",
+                "2",
                 "--low",
                 Palette.red palette,
+                "-f",
+                "<fn=1>\xf244\xf243\xf243\xf242\xf242\xf242\xf241\xf241\xf241\xf240</fn>",
                 "--",
-                "-O",
-                "<icon=battery-mid-charging.xpm/>",
-                "-i",
-                "<icon=battery-charging.xpm/>",
-                "-o",
-                "<icon=battery-mid.xpm/>"
+                "--on",
+                "<fn=1> \xf1e6</fn>", -- Battery charging progress
+                "--idle",
+                "<fn=1> \xf14a</fn>", -- Battery charging done
+                "--off",
+                "<fn=1> \xf240</fn>" -- Battery mid
               ]
               50,
+          -- There's something of an IconPattern class we can use to
+          -- potentially provide different icons for signal strength.
           Run $
             Wireless
               "wlp0s20f3"
               [ "--template",
-                "<icon=connected.xpm/> <essid> <quality>",
+                " <fn=1>\xf1eb</fn> <essid> <quality>",
                 "--high",
                 Palette.green palette,
                 "--normal",
@@ -76,7 +77,7 @@ config palette =
                 Palette.red palette
               ]
               30,
-          Run $ Date "<icon=calendar.xpm/> %R %a %Y-%m-%d " "date" 1,
+          Run $ Date " %R %a %Y-%m-%d " "date" 1,
           Run $
             Volume
               "default"
@@ -85,9 +86,9 @@ config palette =
                 "<status> <volume>%",
                 "--",
                 "-O",
-                "<icon=volume-on.xpm/>",
+                "<fn=1>\xf028</fn>",
                 "-o",
-                "<icon=volume-muted.xpm/>"
+                "<fn=1>\xf6a9</fn>"
               ]
               10
         ]
